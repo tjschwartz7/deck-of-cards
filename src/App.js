17477b4dd1566857e65eb571cfe00b0f2a504cd5
Trenton Schwartz <tj.schwartz2000@gmail.com>
2022-01-12 16:52:47 -0500

    Created components for card, table (board), and Screen

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-      <html lang="en">
+    <html lang="en">
 
-        <head>
-          <meta charset="UTF-8"/>
-          <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-          <title>Deck of Cards</title>
-        </head>
-        <body>
-          <main class="wrapper">
-            <header class="header">
-              <div class="header__draw"></div>
-              <div class="header__discord"></div>
-            </header>
-            <footer class="footer">
-              <div class="footer__cards">
-                <div class="footer__cards--one" style={cardStyle}></div>
-                <div class="footer__cards--two" style={cardStyle}></div>
-                <div class="footer__cards--three" style={cardStyle}></div>
-                <div class="footer__cards--four" style={cardStyle}></div>
-                <div class="footer__cards--five" style={cardStyle}></div>
-              </div>
-            </footer>
-          </main>
-        </body>
+      <head>
+        <meta charset="UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>Deck of Cards</title>
+      </head>
+      <body>
+        <main class="wrapper">
+          <header class="header">
+            <div class="header__draw"></div>
+            <div class="header__discord"></div>
+          </header>
 
-      </html>
+        </main>
+      </body>
+
+    </html>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,19 +2,94 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { useState } from "react";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+<Screen/>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
 
+//function API(){
+  //const API_KEY = 'https://api.nasa.gov/planetary/apod?api_key=exXD91W9CP7AvXGNTI0ilBji1HwQ75hbhw8AzFLY';
 
-const API_KEY = 'https://api.nasa.gov/planetary/apod?api_key=exXD91W9CP7AvXGNTI0ilBji1HwQ75hbhw8AzFLY';
+  //const [cards, setCards] = useState();
+
+  //const getApiData = async () => {
+  //  const response = await fetch(API_KEY).then(
+      //(response) => response.JSON()
+  //  );
+
+    //setCards(response);
+  //}
+//}
+
+
+
+function Card(props){
+
+  return(
+    <div className={props.class_name} backgroundImage={props.image}><p>{props.class_name}</p></div>
+
+  )
+}
+
+class Table extends React.Component {
+
+  renderCard(data, i) {
+    return (
+      <Card
+      value = {data}
+      class_name= {"footer__cards--" + data}
+      image={this.props.cards[i]}
+      //onClick={() => this.props.onClick(data)}
+      />
+    );
+  }
+
+
+  render() {
+    return (
+      <div class="footer__cards">
+          {this.renderCard("one", 0)}
+          {this.renderCard("two", 1)}
+          {this.renderCard("three", 2)}
+          {this.renderCard("four", 3)}
+          {this.renderCard("five", 4)}
+          </div>
+
+    );
+  }
+}
+
+
+class Screen extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      //ANY AND ALL INITIALIZATIONS OF DATA GO HERE
+      cards: Array(5).fill(null)
+    };
+
+  }
+  handleClick(i){
+    //Do something if cards[i] is clicked
+  }
+render(){
+  return(
+    <footer class="footer">
+
+    <App/>
+    <Table/>
+  </footer>
+  );
+}
+
+
+
+}
 
 
 
