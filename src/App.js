01dc5d5010d85ab64c52828668459555cc85d784
Trenton Schwartz <tj.schwartz2000@gmail.com>
2022-01-14 16:34:52 -0500

    Connected to nasa api, took in data and then displayed it on cards

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 
 import './App.css';
 import React from 'react';
+import axios from 'axios';
 
 function App(){
+
+  const API_KEY = 'https://api.nasa.gov/planetary/apod?count=5&api_key=exXD91W9CP7AvXGNTI0ilBji1HwQ75hbhw8AzFLY';
+  const[image, setImage] = React.useState(null);
+  var count = 0;
+  const numbers = [0,1,2,3,4];
+console.log(5);
+
+React.useEffect(() => {
+  axios.get(API_KEY).then((response) => {
+    setImage(response.data);
+  });
+}, []);
+if(image != null)
+  console.log(image[0].url);
+
     return(
       <div className="container">
 
@@ -10,9 +26,14 @@ function App(){
         <div className="header__draw"></div>
         <div className="header__discord"></div>
       </div>
-        <div class="footer">
+        <div className="footer">
         <Table
+        imageArr = {image == null ? null : Array.from([0,1,2,3,4], x=>image[x])}
 
+        //image2 = {(image == null) ? null : image[1].url}
+        //image3 = {(image == null) ? null : image[2].url}
+        //image4 = {(image == null) ? null : image[3].url}
+        //image5 = {(image == null) ? null : image[4].url}
         />
       </div>
     </div>
@@ -23,15 +44,16 @@ export default App;
 
 
 function Card(props){
-
+    {console.log(props.image)}
   return(
-    <div className={props.class_name}></div>
+    <img className={props.class_name} src={(props.image==null) ? "" : props.image.url} onClick={props.onClick}></img>
 
   )
 }
 
 class Table extends React.Component {
   constructor(props){
+
     super(props);
     this.state = {
       //ANY AND ALL INITIALIZATIONS OF DATA GO HERE
@@ -40,15 +62,19 @@ class Table extends React.Component {
       class_name: ""
 
     };
-
   }
   handleClick(i){
     //Do something if cards[i] is clicked
+    {this.renderCard("one", i)}
+    {console.log(69)}
+
   }
   renderCard(data, i) {
     return (
       <Card
-      class_name= {"footer__cards--one"}
+      class_name= {"footer__cards--"+data}
+      image={this.props.imageArr == null ? null : this.props.imageArr[i]}
+      onClick={() => this.handleClick(i)}
       />
     );
   }
@@ -66,4 +92,5 @@ class Table extends React.Component {
 
     );
   }
+
 }
